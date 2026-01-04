8ae4fef16ef8c981b77575e28195e50f14f55a99
PrintTrd <printtheerada@gmail.com>
2026-01-05 00:21:05 +0700

    build: require dagster-webserver

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from setuptools import find_packages, setup
 setup(
     name="insurance_loss_ratio",
     version="0.0.1",
-    packages=find_packages(exclude=["insurance_loss_ratio_tests"]),
-    
+    packages=find_packages(exclude=["medical_cost_dbt", "tests"]),
     install_requires=[
         "dagster",
         "dagster-cloud",
         "dagster-dbt",
+        "dagster-postgres",
+        "dagster-webserver",
         "dbt-core",
         "dbt-postgres",
         "pandas",
-        "psycopg2-binary", # driver for postgres
         "duckdb",
-        "faker",           # for mock pii
-        "python-dotenv",   # load .env
+        "faker",  # for mock pii
+        "psycopg2-binary",  # driver for postgres
+        "python-dotenv",  # load .env
+        "setuptools<70",
         "sqlalchemy",
     ],
-
     # Library for Development Environment
     extras_require={
         "dev": [
-            "dagster-webserver", 
             "pytest",
         ]
     },
